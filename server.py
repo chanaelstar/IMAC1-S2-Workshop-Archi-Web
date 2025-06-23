142e09335bb49cbfecdca2d5ed287bfe683a6bb1
Emilie-Des <emiliedesombre77@gmail.com>
2025-06-23 10:07:33 +0200

    import mysql dans le python

--- server.py
+++ server.py
@@ -1,6 +1,15 @@
 from flask import Flask, render_template, request, jsonify
+from flask_cors import CORS
 import random 
-myapp = Flask("--name--")
+import mysql.connector
+myapp = Flask(__name__)
+
+mydB = mysql.connector.connect (
+    host="localhost",
+    user="root",
+    #password="",
+    #database="test"
+)
 
 @myapp.route("/")
 def formulaire():

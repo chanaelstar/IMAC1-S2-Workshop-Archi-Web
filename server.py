e1c3337036f899d4f12e40cd49d2bb6629dfe788
chanaelstar <chanaelstar@gmail.com>
2025-06-23 11:11:19 +0200

    modification fonction ajout + affichage étudiant

--- server.py
+++ server.py
@@ -11,17 +11,25 @@ mydB = mysql.connector.connect (
     #database="test"
 )
 
+default = {"Numéro étudiant": "1", "Nom": "Dupont", "Prénom": "Jean"}
+
 @myapp.route("/")
 def accueil():
     return render_template('accueil.html')
 
 @myapp.route("/liste_etudiants")
 def affichage():
-    return render_template("affichage_etud.html")
+    return render_template("affichage_etud.html", 
+                           etudiants=[default, default, default, default])
 
 
-@myapp.route("/ajout")
+
+@myapp.route("/ajout", methods=['GET', 'POST'])
 def ajout():
+    num_etudiant = request.form["num_etudiant"]
+    nom = request.form["nom"]
+    prenom = request.form["prenom"]
+
     return affichage()
 
 @myapp.route("/suppression")

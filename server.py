bc869a4de8125aab2e6e73eacfd51251c8271180
Emilie-Des <emiliedesombre77@gmail.com>
2025-06-24 11:41:17 +0200

    SQL correspondant au schéma relationnel défini

--- server.py
+++ server.py
@@ -28,23 +28,22 @@ liste_etudiants_talents = [
 mycursor =mydB.cursor()
 
 # Création des tables
-mycursor.execute('''create table IF NOT EXISTS etudiants (id int primary key auto_increment, prenom varchar(50), nom varchar(50)
+mycursor.execute('''create table IF NOT EXISTS etudiant (id_num int primary key auto_increment, prenom varchar(50), nom varchar(50), id_groupe int (10),
+                 foreign key(id_groupe) references groupe(id_grp)
                  )''')
-mycursor.execute('''create table IF NOT EXISTS talents (id int primary key auto_increment, nom varchar(50)
-                 )''')
-mydB.commit()
-mycursor.execute('''create table IF NOT EXISTS groupes (id int primary key auto_increment, id_membre int(10), nb_membres int
+mycursor.execute('''create table IF NOT EXISTS talent (id_tal int primary key auto_increment, nom varchar(50)
                  )''')
 mydB.commit()
-mycursor.execute('''create table IF NOT EXISTS projets (id int primary key auto_increment, nom varchar(50), id_groupe int(10), foreign key(id_groupe) references groupes (id)
+mycursor.execute('''create table IF NOT EXISTS groupe (id_grp int primary key auto_increment, nb_membres int, id_projet int,
+                 foreign key(id_projet) references projet(id_prj)
+                 )''')
+mydB.commit()
+mycursor.execute('''create table IF NOT EXISTS projet (id_prj int primary key auto_increment, nom varchar(50), id_groupe int(10),
+                 foreign key(id_groupe) references groupe (id_grp)
                  )''')
 mydB.commit()
 mycursor.execute('''create table IF NOT EXISTS possede (id_etud int, id_talent int, primary key(id_etud, id_talent),
-                 foreign key(id_etud) references etudiants(id), foreign key(id_talent) references talents(id)
-                 )''')
-mydB.commit()
-mycursor.execute('''create table IF NOT EXISTS forme (id_etud int, id_groupe int, primary key(id_etud, id_groupe),
-                 foreign key(id_groupe) references groupes(id), foreign key(id_etud) references etudiants(id)
+                 foreign key(id_etud) references etudiant(id_num), foreign key(id_talent) references talent(id_tal)
                  )''')
 mydB.commit()
 mycursor.execute('''create table IF NOT EXISTS controle(id_table int primary key auto_increment, nom_table varchar (10), implement bool default(False))
@@ -60,8 +59,8 @@ if isAlreadyDone is None :
 
 if not isAlreadyDone :
     mycursor.execute(''' insert into controle (nom_table, implement) values
-                    ('talents', False),
-                    ('etudiants',False),
+                    ('talent', False),
+                    ('etudiant',False),
                     ('controle',True);''')
     mydB.commit()
 
@@ -70,7 +69,7 @@ mycursor.execute('''select implement from controle
 isAlreadyDone = mycursor.fetchone()[0]
 
 if not isAlreadyDone:
-    mycursor.execute(''' insert into talents (nom) values
+    mycursor.execute(''' insert into talent (nom) values
                     ('dev'),
                     ('musique'),
                     ('dessin'),
@@ -87,7 +86,7 @@ mycursor.execute('''select implement from controle
 isAlreadyDone = mycursor.fetchone()[0]
 
 if not isAlreadyDone:
-    mycursor.execute(''' insert into etudiants (prenom, nom) values
+    mycursor.execute(''' insert into etudiant (prenom, nom) values
                     ('Bob','leponge'),
                     ('Dora','lexploratrice'),
                     ('koro','sensei');''')
@@ -97,19 +96,19 @@ if not isAlreadyDone:
                     where id_table=2;''')
     mydB.commit()
 
-mycursor.execute('''select * from etudiants''')
+mycursor.execute('''select * from etudiant''')
 etud=mycursor.fetchall()
 print(etud)
 
-mycursor.execute('''select * from talents''')
+mycursor.execute('''select * from talent''')
 tal=mycursor.fetchall()
 print(tal)
 
-mycursor.execute('''select * from groupes''')
+mycursor.execute('''select * from groupe''')
 grp=mycursor.fetchall()
 print(grp)
 
-mycursor.execute('''select * from projets''')
+mycursor.execute('''select * from projet''')
 prj=mycursor.fetchall()
 print(prj)
 
@@ -117,16 +116,12 @@ mycursor.execute('''select * from controle''')
 ctrl=mycursor.fetchall()
 print(ctrl)
 
-mycursor.execute('''select * from forme''')
-frm=mycursor.fetchall()
-print(frm)
-
 mycursor.execute('''select * from possede''')
 pssd=mycursor.fetchall()
 print(pssd)
 
 mycursor.close()
-# Fin SQL
+###### Fin SQL ######
 
 @myapp.route("/")
 def accueil():
